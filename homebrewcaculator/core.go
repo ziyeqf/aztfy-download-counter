240610944b0052b276a7bad418d9ebf51fa56ea2
magodo <wztdyl@sina.com>
2023-03-24 17:03:03 +0800

    Collaboratedly going through and change

--- homebrewcaculator/core.go
+++ homebrewcaculator/core.go
@@ -2,38 +2,10 @@ package homebrewcaculator
 
 type mockDBClient struct{}
 
-func (m mockDBClient) IsAValidDate(date Date) bool {
-	return true
+func (m mockDBClient) Get(int) (CountInfo, error) {
+	return CountInfo{}, nil
 }
 
-func (m mockDBClient) GetDataAtDayN(date Date) (Data, error) {
-	return Data{}, nil
-}
-
-func (m mockDBClient) UpdateDataAtDayN(date Date, data Data) error {
+func (m mockDBClient) Update(idx int, data CountInfo) error {
 	return nil
-}
-
-func Start(startDate Date) {
-	client := mockDBClient{}
-
-	queue := Queue{}
-	for _, span := range PossibleSpans() {
-		queue.Enqueue(calculateDayCountAtDayN(startDate, span))
-		queue.Enqueue(calculateHistoryCountAtDayN(startDate, span))
-		queue.Enqueue(calculateHistoryCountAtDayNByNewerData(startDate, span))
-		queue.Enqueue(calculateDayCountAtDayNByNewerData(startDate, span))
-	}
-
-	for !queue.IsEmpty() {
-		calculateFuncPtr := queue.Dequeue()
-		if calculateFuncPtr == nil {
-			calculateFunc := *calculateFuncPtr
-			newFuncs, err := calculateFunc(client)
-			if err != nil {
-				panic(err)
-			}
-			queue.Enqueue(newFuncs...)
-		}
-	}
 }
--- homebrewcaculator/database.go
+++ homebrewcaculator/database.go
@@ -1,9 +1,14 @@
 package homebrewcaculator
 
-const RecordStartDate = "2023-03-24" // data before this date should be filled with zero value
+// CountInfo represents the count information of a certain index that is recorded by the DatabasesClient.
+type CountInfo struct {
+	// Count represents the count at this index. Especially, -1 represents there is no count at this index.
+	Count int32
+	// TotalCounts represents the cumulative counts of each span.
+	TotalCounts map[Span]int32
+}
 
 type DatabaseClient interface {
-	IsAValidDate(date Date) bool                 //if the date is not valid?
-	GetDataAtDayN(date Date) (Data, error)       // do not return error when there is no record.
-	UpdateDataAtDayN(date Date, data Data) error // add a new record or update the existing record.
+	Get(idx int) (CountInfo, error)    // do not return error when there is no record.
+	Set(idx int, data CountInfo) error // add a new record or update the existing record.
 }
